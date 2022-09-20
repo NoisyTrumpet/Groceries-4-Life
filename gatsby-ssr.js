3633fb373d074c6a4013c53e241669e727f1124d
Cesar Benavides <cesarbenavides@icloud.com>
2022-09-20 16:09:52 -0500

    more tweaks to proxy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,8 @@ const resolveUrl = (url) => {
     url.hostname === "www.google-analytics.com" ||
     url.hostname === "connect.facebook.net" ||
     url.hostname === "analytics.tiktok.com" ||
-    url.hostname === "www.googletagmanager.com"
+    url.hostname === "www.googletagmanager.com" ||
+    url.hostname === "www.googleadservices.com"
   ) {
     var proxyUrl = new URL(
       `https://coop-atm.mygenfcu.workers.dev/?${url.href}`
@@ -34,10 +35,10 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
       type="text/partytown"
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${process.env.GATSBY_GOOGLE_TAG_MANAGER_ID}');`,
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GATSBY_GOOGLE_TAG_MANAGER_ID}');`,
       }}
     />,
   ]),
